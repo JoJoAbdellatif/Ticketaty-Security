01c302bfa3e812ec888025e8f921feedc1f310a9
XXXJoJoXXX <ymostafa2010@gmail.com>
2022-12-25 15:52:47 +0200

    changed rate limiter

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,13 +1,11 @@
 const rateLimit = require('express-rate-limit');
 
-const rateLimiter = (req, res, next) => {
-  rateLimit({
-    windowMs: 15 * 60 * 1000, // 24 hrs in milliseconds
-    max: 10,
-    message: 'You have exceeded the 100 requests in 24 hrs limit!', 
-    standardHeaders: true,
-    legacyHeaders: false,
-  });
-  next()
-}
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 24 hrs in milliseconds
+  max: 10,
+  message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 module.exports = {rateLimiter};
