936ed863e6afc9224e232fc5f5196c816c0ab015
XXXJoJoXXX <ymostafa2010@gmail.com>
2022-12-27 19:34:40 +0200

    added endpoints

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
 const { isIPBlocked } = require('./middlewares/IPBlocking');
+const {corsHeaders} = require('./middlewares/cors');
+const axios = require('axios');
+const asyncHandler = require('express-async-handler')
 
 
 const app = express();
@@ -11,10 +14,46 @@ const limiter = rateLimit({
     message : "Too many requests, please try later"
 })
 
-app.get('/' , isIPBlocked , limiter , (req , res) => {
-    res.send("alooooooo")
+app.get('/matches', isIPBlocked , limiter , corsHeaders, asyncHandler(async(req , res) => {
+    const par = req.query.p
+    const url = "https://ticketaty-shop.vercel.app/matches/?p=" + par
+    await axios.get(url , { 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    })
+    .then((response) => (res.send(response.data)))
+    .catch((e) => {res.send(e)})
+}))
+
+app.get('/matches/:id' , isIPBlocked , limiter , corsHeaders , asyncHandler(async (req , res) => {
+    const par = req.params.id
+    const url = "https://ticketaty-shop.vercel.app/matches/" + par
+    await axios.get(url , { 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    })
+    .then((response) => (res.send(response.data)))
+    .catch((e) => {res.send(e)})
+}))
+
+app.get('/search/:team' , isIPBlocked , limiter , corsHeaders , asyncHandler(async (req , res) => {
+    const par = req.params.team
+    const url = "https://ticketaty-shop.vercel.app/search/" + par
+    await axios.get(url , { 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    })
+    .then((response) => (res.send(response.data)))
+    .catch((e) => {res.send(e)})
     //axios call to actual endpoints except pending
-})
+}))
+
+app.get('/reservation' , isIPBlocked , limiter , corsHeaders , asyncHandler(async (req , res) => {
+    const url = "https://ticketaty-reservations.vercel.app/api/v1/health"
+    await axios.get(url , { 
+        headers: { "Accept-Encoding": "gzip,deflate,compress" }
+    })
+    .then((response) => (res.send(response.data)))
+    .catch((e) => {res.send(e)})
+    //axios call to actual endpoints except pending
+}))
 
 app.listen('3000', () => {
     console.log('app listening on port 3000')
